959292e6a796f1ffae1d2f3e3bf1ba600ee9b8dc
GogoGit <6920876+GogoGit@users.noreply.github.com>
2024-04-03 21:01:17 -0400

    Bug Hunting, sometimes you get an [] empty array in localStorage.
    This is what I did to solve the issue??
    
    used Maria Lima's hint... was thinking along the same lines.  Now I don't have to figure out the code :) + moved setLoading(false).
    
    Also update the url if it defaults to 'arts'.
    
    having trouble replicating issue, if I try too many times, the I used my key it too many times give me a different error which is not what I"m trying to debug.

--- all-the-news-vite/src/app.jsx
+++ all-the-news-vite/src/app.jsx
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import Stories from "./components/Stories";
 import { useState, useEffect } from "preact/hooks";
 
+const bolDebug = true;
 const NAVITEMS = ["arts", "books", "fashion", "food", "movies", "travel"];
 const FETCH_URL = "https://api.nytimes.com/svc/topstories/v2/";
 const NYT_API = "PGQuh0auTqHC6HEx4gADBhT2yLCdXYbN";
@@ -10,31 +11,53 @@ const NYT_API = "PGQuh0auTqHC6HEx4gADBhT2yLCdXYbN";
 export function App() {
   const [stories, setStories] = useState([]);
   const [loading, setLoading] = useState(false);
-  // const [section, setSection] = useState("arts");
   // we are testing to see if we have a hash value and if not we are setting the default value to 'arts'
   const [section, setSection] = useState("arts");
 
   useEffect(() => {
     const url = new URL(window.location.href);
-    // console.log(url);
+    if (bolDebug) {
+      console.log(url);
+    }
     const hash = url.hash.slice(1); //we want the url without the hash #
 
-    if (hash !== "undefined") {
+    if (bolDebug) {
+      console.log(
+        `URL hash:  ${hash}, type of ${typeof hash}, length: ${hash.length}`
+      );
+    }
+
+    if (hash !== "undefined" && hash.length !== 0) {
+      if (bolDebug) {
+        console.log(`URL hash Update to:  ${hash}`);
+      }
       setSection(hash);
     } else {
       setSection("arts");
+      window.location.href = "http://localhost:5173/#arts";
+
+      if (bolDebug) {
+        console.log(`URL hash Default to: arts, URL Hash '${url.hash}'`);
+      }
     }
   }, []);
 
+  // if(bolDebug){console.log(``)};
+
   useEffect(() => {
+    if (bolDebug) {
+      console.log(`LocalStorage (start) section: ${section}`);
+    }
     if (!localStorage.getItem(section)) {
       console.log("fetching from NYT");
       setLoading(true);
       fetch(`${FETCH_URL}${section}.json?api-key=${NYT_API}`)
         .then((response) => response.json())
-        .then((data) => setStories(data.results))
+        .then((data) => {
+          setStories(data.results);
+          setLoading(false);
+        })
         .catch((error) => console.error(error));
-      setLoading(false);
     } else {
       console.log("section is in storage, not fetching");
       setStories(JSON.parse(localStorage.getItem(section)));
@@ -53,9 +76,10 @@ export function App() {
   return (
     <>
       {/* Note you can add UI elements here to test your code! view your variables. */}
-
+      <h1>what section is this??? {section}</h1>
       <Header siteTitle="All the News That Fits We Print" />
       <Nav navItems={NAVITEMS} setSection={setSection} section={section} />
+      <p>what section is this??? {section}</p>
       <Stories stories={stories} />
     </>
   );
